62229f4317fca281c34e4dc4a8d07d4e4c10e5f1
ryevdokimov <robert.yevdokimov@autstand.com>
2022-08-04 09:20:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quasarframework-1mmxev
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/quasarframework-1mmxev)
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="column flex-center">
+    <div class="q-mt-xl">
+      <q-btn color="primary" dense round label="-" @click="count--" />
+
+      <span class="q-mx-md text-bold">{{ count }}</span>
+
+      <q-btn color="primary" dense round label="+" @click="count++" />
+    </div>
+
+    <div class="q-pa-md">
+      <q-table title="TableTest" dense :rows="state.rows" row-key="name" />
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { ref, reactive, onMounted } from 'vue';
+
+let count = ref(0);
+
+let state = reactive({ rows: [] });
+
+onMounted(() => {
+  getData();
+});
+
+async function getData() {
+  const response = await fetch(
+    'https://c47d7a57-e2fe-42f1-b9bc-e3dc4e2c6d58.mock.pstmn.io/test'
+  );
+  const data = await response.json();
+
+  state.rows = data;
+}
+</script>
